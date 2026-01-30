e6baa72514a4c12592e095bcf35f5699ff31a075
ramazansartay2 <ramazansartay2@gmail.com>
2026-01-30 02:52:29 +0000

    Fix deployment issues

--- server/static.ts
+++ server/static.ts
@@ -6,7 +6,8 @@ import { fileURLToPath } from "url";
 export function serveStatic(app: Express) {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const distPath = path.resolve(__dirname, "public");
+  const distPath = path.resolve(__dirname, "..", "dist", "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -16,7 +17,7 @@ export function serveStatic(app: Express) {
   app.use(express.static(distPath));
 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  app.use("/*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
