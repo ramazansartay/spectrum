6df74026ec94ce166d09054ddd0153ddf5e6bbfe
ramazansartay2 <ramazansartay2@gmail.com>
2026-02-06 10:24:12 +0000

    fix: import index.css to fix missing styles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { Toaster } from "@/components/ui/toaster"
+import { TooltipProvider } from './components/ui/tooltip.js'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TooltipProvider>
+      <App />
+      <Toaster />
+    </TooltipProvider>
+  </React.StrictMode>,
+)
