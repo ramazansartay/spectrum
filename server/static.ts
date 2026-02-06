3803f043e8d32d4e8c3658520a760d922f262d64
ramazansartay2 <ramazansartay2@gmail.com>
2026-02-06 16:11:06 +0000

    fix: resolve build and runtime errors

--- server/static.ts
+++ server/static.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path';
 export function setupStatic(app: Express) {
   if (process.env.NODE_ENV !== 'production') return;
 
-  const clientPath = resolve('dist/client');
+  const clientPath = resolve('dist/public');
 
   app.use(express.static(clientPath));
 
